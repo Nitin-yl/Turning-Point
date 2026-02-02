7c38409a0df9598b1259f4b3058260635098ab0c
Nitin-Goyal12 <navgoyal321@gmail.com>
2026-02-02 21:42:59 +0530

    learning api

--- Day-3/server.js
+++ Day-3/server.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-})
+app.use(express.json());
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  notes.push(req.body);
+  res.send("Note created");
+});
+
+app.get("/notes", (req, res) => {
+  res.send(notes);
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-})
+  console.log("Server is running on port 3000");
+});
